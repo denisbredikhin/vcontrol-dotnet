f9e129cf93b2d022b8d7e73016ec22731c3d2760
denis.bredikhin <denis.bredikhin@agilefocus.be>
2025-12-24 08:14:32 +0100

    Add logging configuration to Program class for improved diagnostics

--- src/Vcontrol.Worker/Program.cs
+++ src/Vcontrol.Worker/Program.cs
@@ -1,8 +1,19 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 
 var host = Host.CreateDefaultBuilder(args)
+    .ConfigureLogging(logging =>
+    {
+        logging.ClearProviders();
+        logging.AddSimpleConsole(options =>
+        {
+            options.TimestampFormat = "yyyy-MM-dd HH:mm:ss.fff ";
+            options.UseUtcTimestamp = false; // set true if you prefer UTC
+            options.SingleLine = false;
+        });
+    })
     .ConfigureServices(services =>
     {
         services.AddSingleton<Vcontrol.Worker.MqttService>();
